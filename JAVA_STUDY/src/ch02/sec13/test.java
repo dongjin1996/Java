5c6bd90247e26a23f66b844db87097e915a552bb
dongjin1996 <ehwl01@naver.com>
2023-04-21 18:19:33 +0900

    4/21

---
+++ JAVA_STUDY/src/ch02/sec13/test.java
@@ -0,0 +1,11 @@
+package ch02.sec13;
+
+public class test {
+
+	public static void main(String[] args) {
+	
+		char c = '';
+		char answer = 'no';
+	}
+
+}
---
+++ JAVA_STUDY/src/ch03/sec04/AccuracyExample1.java
@@ -0,0 +1,18 @@
+package ch03.sec04;
+
+public class AccuracyExample1 {
+
+	public static void main(String[] args) {
+		//정확한 계산은 정수 연산으로 해야한다.
+		
+		int apple = 1; //사과 1조각
+		int totalPieces = apple * 10; // 10분의1
+		int number = 7;
+		
+		double result = totalPieces - number;  // 10 - 7 = 3
+		System.out.println("사과 1개에서 남은양:" + result/10.0);
+		
+		
+	}
+
+}
