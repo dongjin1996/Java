8d258ffc92508f9b7f38657d442656a1ead912dd
dongjin1996 <ehwl01@naver.com>
2023-05-01 17:58:20 +0900

    5/1

--- JAVA_STUDY/src/question/BankApplication.java
+++ JAVA_STUDY/src/question/BankApplication.java
@@ -90,19 +90,99 @@ public class BankApplication {
 	
 	//계좌목록 보여주기
 	private static void accountList() {
-		/*
-		 * 힌트:
+		System.out.println("------------");
+		System.out.println("계좌목록");
+		System.out.println("------------");
+				
+		
 		for(Account account : accountArray) {
-			System.out.println(account);
+			//배열이 null값일때 실행하는 걸 방지하기 위해
+			if(account != null) {
+				System.out.print(account.getAno());
+				System.out.print(" ");
+				System.out.print(account.getOwner());
+				System.out.print(" ");
+				System.out.print(account.getBalance());
+				System.out.println();
+			}
 		}
-		*/
+	
 	}
 	
 	//예금하기 
-	private static void deposit() {}
+	private static void deposit() {
+		System.out.println("---------------");
+		System.out.println("예금");
+		System.out.println("---------------");
+		
+		//계좌번호, 예금액 입력
+		System.out.println("계좌번호: ");
+		String ano = scanner.next(); //String타입으로 입력을 받는다.
+		
+		System.out.println("예금액: ");
+		int money = scanner.nextInt(); //int타입으로 입력을 받는다.
+		
+		//계좌찾기
+		
+		//리턴해 주는 값을 같은 타입인 account에 받는다.
+		//account 변수에는 찾은 계좌가 들어있다.
+		Account account = findAccount(ano);
+		
+		if(account == null) {
+			System.out.println("결과: 계좌가 없습니다.");
+			return; //메소드를 끝낸다.
+		}
+		
+		account.setBalance(account.getBalance() + money);
+		System.out.println("결과: 예금이 성공되었습니다.");		
+	}
 	
 	//출금하기 
-	private static void withdraw() {}
+	private static void withdraw() {
+		System.out.println("---------------");
+		System.out.println("출금");
+		System.out.println("---------------");
+			
+			//계좌번호, 예금액 입력
+			System.out.println("계좌번호: ");
+			String ano = scanner.next(); //String타입으로 입력을 받는다.
+			
+			System.out.println("출금액: ");
+			int money = scanner.nextInt(); //int타입으로 입력을 받는다.
+			
+			//계좌찾기
+			
+			//리턴해 주는 값을 같은 타입인 account에 받는다.
+			//account 변수에는 찾은 계좌가 들어있다.
+			Account account = findAccount(ano);
+			
+			if(account == null) {
+				System.out.println("결과: 계좌가 없습니다.");
+				return; //메소드를 끝낸다.
+			}
+			
+			account.setBalance(account.getBalance() - money);
+			System.out.println("결과: 출금이 성공되었습니다.");
+	}
 	
 	
+
+	//계좌찾기 메소드
+	private static Account findAccount(String ano) {
+		Account resultAccount = null;
+		for(Account account : accountArray) {
+			//배열의 길이 null이 아닐때만 실행한다. => account 객체를 가지고 있을때
+			if(account != null) {				
+				if (ano.equals(account.getAno())) {
+					//여기서 return을 하면 안되므로 아래와 같이 찾은 계좌를
+					//account 타입의 변수인 resultAccount에 넣어준다.
+					resultAccount = account;
+					break;
+				}
+			}
+		}
+		
+		return resultAccount; //찾은 계좌를 리턴해준다.
+	}
+
 }
