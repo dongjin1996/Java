055a8187ff0a5ca438c4c188383408560c778592
dongjin1996 <ehwl01@naver.com>
2023-05-19 13:17:27 +0900

    5/19

---
+++ JAVA_STUDY/src/test3/for2.java
@@ -0,0 +1,15 @@
+package test3;
+
+public class for2 {
+
+	public static void main(String[] args) {
+
+		int[] arr = {10, 20, 30, 40, 50};
+		int sum = 0;
+		for(int i=0; i<arr.length; i++) {
+			sum += arr[i];
+		}
+		System.out.println(sum);
+	}
+
+}
---
+++ JAVA_STUDY/src/test3/for3.java
@@ -0,0 +1,18 @@
+package test3;
+
+public class for3 {
+
+	public static void main(String[] args) {
+		int[] array = {1, 5, 3, 8, 2};
+		int max = 0;
+		for(int i=0; i<array.length; i++) {
+			if(max < array[i]) {
+				max = array[i];
+			}
+		}
+		System.out.println("최대값: " + max);
+		
+		
+	}
+
+}
